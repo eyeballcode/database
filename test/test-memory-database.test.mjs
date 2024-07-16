cd8b587276d7e1090435b1ffc40572ba9e2796dc
Eyeball Edward <edwardyeung39@gmail.com>
2024-07-16 16:09:29 +1000

    add some support for geospatial queries

--- test/test-memory-database.test.mjs
+++ test/test-memory-database.test.mjs
@@ -12,4 +12,39 @@ describe('The in memory database', () => {
     expect((await coll.findDocument({ a: 1 })).b).to.equal(1)
     expect((await coll.findDocument({ a: 2 })).b).to.equal(2)
   })
+
+  it('Should transform geospatial queries using $where and turf', async () => {
+    let db = new LokiDatabaseConnection('test-db')
+
+    let coll = await db.createCollection('test-coll')
+    await coll.createDocument({
+      "stopName": "Dole Avenue/Cheddar Road",
+      "location": {
+        "type": "MultiPoint",
+        "coordinates": [[ 145.018951051008, -37.7007748061827 ]]
+      }
+    })
+    await coll.createDocument({
+      "stopName": "Rex Street/Taylors Road",
+      "location": {
+        "type": "MultiPoint",
+        "coordinates": [[ 144.776152425766, -37.7269752097338 ]]
+      }
+    })
+
+    let stop = await coll.findDocument({
+      location: {
+        $nearSphere: {
+          $geometry: {
+            "type": "Point",
+            "coordinates": [ 145.018954051418, -37.7007758062827 ]
+          },
+          $maxDistance: 500
+        }
+      }
+    })
+
+    expect(stop).to.not.be.null
+    expect(stop.stopName).to.equal('Dole Avenue/Cheddar Road')
+  })
 })
